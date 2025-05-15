be0d7e1c72302c8cf02cbc911814cdc0a43ad306
Elish <medioni@g.jct.ac.il>
2025-05-15 19:53:42 +0300

    Initial Commit

---
+++ HarryPotterProject/HarryPotterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterProjectApp.swift
+//  HarryPotterProject
+//
+//  Created by Elisheva Medioni on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
